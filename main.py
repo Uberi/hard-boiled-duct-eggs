6879f9ed456466d033979c76785e303fe40a5712
Anthony Zhang <azhang9@gmail.com>
2017-11-12 14:07:59 -0500

    AAA

--- main.py
+++ main.py
@@ -2,22 +2,39 @@
 
 import face_recognition
 import cv2
+from numpy import array
+import numpy as np
 
 from PIL import Image, ImageDraw
 
 WEBCAM_INDEX = 0
+DEBUG = True
 
 video_capture = cv2.VideoCapture(WEBCAM_INDEX)
+
+def find_eye_outer_corners(left_eye_points, right_eye_points):
+    face_axis = np.mean(right_eye_points, axis=0) - np.mean(left_eye_points, axis=0)
+    left_index, right_index = np.argmin(left_eye_points @ face_axis), np.argmax(right_eye_points @ face_axis)
+    return left_eye_points[left_index], right_eye_points[right_index]
+
+def ndarray_to_coordinate(ndarray):
+    return int(ndarray[0]), int(ndarray[1])
 
 while True:
     status, frame = video_capture.read()
     assert status
 
-    face_landmarks_list = face_recognition.face_landmarks(image)
+    face_landmarks_list = face_recognition.face_landmarks(frame)
 
     for face_landmarks in face_landmarks_list:
-        d.line(face_landmarks[facial_feature], width=5)
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        left_eye, right_eye = array(face_landmarks["left_eye"]), array(face_landmarks["right_eye"])
+        left_corner, right_corner = find_eye_outer_corners(left_eye, right_eye)
+
+        if DEBUG:
+            cv2.line(frame, ndarray_to_coordinate(left_corner), ndarray_to_coordinate(right_corner), (0, 0, 255), 4)
+
+        #wip: add drawing code here based on left_corner, right_corner
+
 
     # Display the resulting image
     cv2.imshow('Video', frame)
